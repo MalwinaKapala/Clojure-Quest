dd641cb99c28f5502c27133826dfba98c31737a2
Malwina Kapala <malwina.gadek@gmail.com>
2017-09-07 13:41:05 +0200

    onClicked method on radio buttons added.

--- app/src/main/java/com/example/malwina/clojurequest/MainActivity.java
+++ app/src/main/java/com/example/malwina/clojurequest/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.malwina.clojurequest;
 
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-}
+
+    public void onRadioButtonClicked (View view) {
+        RadioButton rb1 = (RadioButton) findViewById(R.id.radio_button_1);
+        RadioButton rb2 = (RadioButton) findViewById(R.id.radio_button_2);
+        RadioButton rb3 = (RadioButton) findViewById(R.id.radio_button_3);
+        RadioButton rb4 = (RadioButton) findViewById(R.id.radio_button_4);
+
+        boolean checked = ((RadioButton)view).isChecked();
+
+        switch (view.getId()){
+    }
+}}
