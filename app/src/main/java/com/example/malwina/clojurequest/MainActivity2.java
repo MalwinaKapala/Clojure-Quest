80790899995cf994af129b7945eb2d961f04f2cd
Malwina Kapala <malwina.gadek@gmail.com>
2018-08-21 13:42:07 +0200

    new class QuestionCard added; questioncards with answears added

---
+++ app/src/main/java/com/example/malwina/clojurequest/MainActivity2.java
@@ -0,0 +1,8 @@
+package com.example.malwina.clojurequest;
+
+/**
+ * Created by malwina on 08.08.18.
+ */
+
+public class MainActivity2 {
+}
---
+++ app/src/main/java/com/example/malwina/clojurequest/QuestionCard.java
@@ -0,0 +1,8 @@
+package com.example.malwina.clojurequest;
+
+/**
+ * Created by malwina on 17.08.18.
+ */
+
+public class QuestionCard {
+}
